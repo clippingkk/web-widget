55ae405e10d371c0c74cb0445b01ad454d806d2f
AnnatarHe <annatar.he+github@gmail.com>
2025-05-31 01:20:26 +0800

    fix(type): update React import

--- type.d.ts
+++ type.d.ts
@@ -1,13 +1,13 @@
-import React from 'react'
+import * as React from 'react'
 
-declare global {
-    namespace JSX {
-      interface IntrinsicElements {
-        'clippingkk-web-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-          clippingId?: number;
-          theme?: 'light' | 'dark';
-          endpoint?: string;
-        };
-      }
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'clippingkk-web-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        clippingId?: number;
+        theme?: 'light' | 'dark';
+        endpoint?: string;
+      };
     }
+  }
 }
